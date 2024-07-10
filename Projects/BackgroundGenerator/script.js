9a1d5e367dffd5f0a0237a4d95727c69e9b41784
TroyKDuncan <troyduncanbusiness@gmail.com>
2024-07-09 22:21:44 -0500

    Switching to desktop

---
+++ Projects/BackgroundGenerator/script.js
@@ -0,0 +1,31 @@
+var css = document.querySelectorAll("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.querySelector("body");
+var randomize = document.querySelector(".random");
+
+function changeColor() {
+  body.style.background =
+    "linear-gradient(to right," + color1.value + ", " + color2.value + ")";
+  css[0].textContent = "Left Color: " + color1.value;
+  css[1].textContent = "Right Color: " + color2.value;
+}
+
+function getRandomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function randomizeColors() {
+  color1.value = getRandomColor();
+  color2.value = getRandomColor();
+  changeColor();
+}
+
+color1.addEventListener("input", changeColor);
+color2.addEventListener("input", changeColor);
+randomize.addEventListener("click", randomizeColors);
