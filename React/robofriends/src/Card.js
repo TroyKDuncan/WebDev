5e274d3b032104b1fcb25830f472b25692a8cc66
TroyKDuncan <troyduncanbusiness@gmail.com>
2024-07-30 16:27:41 -0500

    Working on Robofriends. Currently working on search box feature. Moving over to PC

--- React/robofriends/src/Card.js
+++ React/robofriends/src/Card.js
@@ -1,19 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Card extends Component {
-  render() {
-    const { id, name, username, email } = this.props;
-
-    return (
-      <div className="bg-light-green dib br3 tc pa3 ma2 grow bw2 shadow-5">
-        <img alt="robot" src={`https://robohash.org/${name}?200x200`} />
-        <div>
-          <h2>{name}</h2>
-          <p>{email}</p>
-        </div>
+const Card = ({ name, email }) => {
+  return (
+    <div className="bg-light-green dib br3 tc pa3 ma2 grow bw2 shadow-5">
+      <img alt="robot" src={`https://robohash.org/${name}?200x200`} />
+      <div>
+        <h2>{name}</h2>
+        <p>{email}</p>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Card;
--- React/robofriends/src/index.js
+++ React/robofriends/src/index.js
@@ -2,31 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import Card from "./Card.js";
-import { robots } from "./robots.js";
+import App from "./App.js";
 import "tachyons";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Card
-      id={robots[0].id}
-      name={robots[0].name}
-      username={robots[0].username}
-      email={robots[0].email}
-    />
-    <Card
-      id={robots[1].id}
-      name={robots[1].name}
-      username={robots[1].username}
-      email={robots[1].email}
-    />
-    <Card
-      id={robots[2].id}
-      name={robots[2].name}
-      username={robots[2].username}
-      email={robots[2].email}
-    />
+    <App />
   </React.StrictMode>
 );
 
