ef2a987d16b17d9f227ce2818b3eace607b6233d
TroyKDuncan <troyduncanbusiness@gmail.com>
2024-08-01 15:23:14 -0500

    Finished RoboFriends! React is the best

--- React/robofriends/src/components/Card.js
+++ React/robofriends/src/components/Card.js
@@ -3,7 +3,7 @@ import React from "react";
 const Card = ({ name, email }) => {
   return (
     <div className="bg-light-green dib br3 tc pa3 ma2 grow bw2 shadow-5">
-      <img alt="robot" src={`https://robohash.org/${name}?200x200`} />
+      <img alt="robot" src={`https://robohash.org/${name}?100x100`} />
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
