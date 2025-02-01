8f8ff5ebaa6945a1e4ba7c01a15f207cbca943cd
TroyKDuncan <troyduncanbusiness@gmail.com>
2025-01-31 23:11:40 -0600

    Switching to PC

--- Angular/ComponentFundamentals/basics/src/app/app.component.ts
+++ Angular/ComponentFundamentals/basics/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  // template: `<p>Hello world!</p>`,
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  // styles: [''],
 })
 export class AppComponent {
-  title = 'basics';
+  name = 'nora'
 }
